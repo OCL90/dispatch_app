6fa1b97438f2c5e4b37334d044e894469957e3cc
OCL90 <oscarcortazar90@gmail.com>
2020-06-17 14:05:55 -0500

    added static root

--- dispatch/settings.py
+++ dispatch/settings.py
@@ -122,11 +122,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_URL = '/images/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
